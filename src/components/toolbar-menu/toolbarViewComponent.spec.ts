a4a525e58da98a64c7dcbd91f76013e5bf1a7c42
Karel Hala <khala@redhat.com>
2016-08-30 16:20:17 +0200

    Create tests for toolbar view component

--- src/components/toolbar-menu/toolbarViewComponent.spec.ts
+++ src/components/toolbar-menu/toolbarViewComponent.spec.ts
@@ -7,9 +7,12 @@ describe('ToolbarButton test', () =>  {
     expect(toolbarView).toBeDefined();
   });
 
-  describe('directive', () => {
+  describe('component', () => {
     let scope,
-      compile;
+      compile,
+      compiledElement;
+
+    const toolbarViewData = require<string>('./toolbarView.json');
 
     beforeEach(() => {
       angular.mock.module('miqStaticAssets');
@@ -17,11 +20,39 @@ describe('ToolbarButton test', () =>  {
         scope = $rootScope.$new();
         compile = $compile;
       });
+      scope.toolbarViews = toolbarViewData;
+      compiledElement = compile(
+        angular.element(
+          `<miq-toolbar-view toolbar-views="toolbarViews"
+                             on-item-click="onViewClick({item: item, $event: $event})"
+                             class="miq-view-list"></miq-toolbar-view>`
+        ))(scope);
+      scope.$digest();
     });
 
-    it('creates toolbar view', () => {
-      let compiledElement = compile()(scope);
-      scope.$digest();
+    it('should create toolbar view', () => {
+      const contentElement = angular.element(compiledElement.html());
+      expect(contentElement.hasClass('toolbar-pf-view-selector')).toBe(true);
+      expect(contentElement.hasClass('pull-right')).toBe(true);
+      expect(contentElement.hasClass('form-group')).toBe(true);
+    });
+
+    it('should create 3 views', () => {
+      const contentElement = angular.element(compiledElement.html());
+      expect(contentElement.find('li').length).toBe(3);
+    });
+
+    it('each view should have correct settings', () => {
+      angular.forEach(compiledElement.find('li'), (element, key) => {
+        const liElem = angular.element(element);
+        const linkElem = angular.element(liElem.html());
+        expect(liElem.hasClass('active')).toBe(!!scope.toolbarViews[key].selected);
+        expect(linkElem.attr('title')).toBe(scope.toolbarViews[key].title);
+        expect(linkElem.attr('id')).toBe(scope.toolbarViews[key].id);
+        expect(linkElem.attr('data-url')).toBe(scope.toolbarViews[key].url);
+        expect(linkElem.attr('data-url_parms')).toBe(scope.toolbarViews[key].url_parms);
+        expect(linkElem.attr('name')).toBe(scope.toolbarViews[key].name);
+      });
     });
   });
 });

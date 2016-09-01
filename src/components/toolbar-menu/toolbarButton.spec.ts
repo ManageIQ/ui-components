3b5b1ce31a3d17dbc9c55eed46b53134ef86b7c0
Karel Hala <khala@redhat.com>
2016-09-01 17:31:18 +0200

    Add testing of toolbar button text to be present

--- src/components/toolbar-menu/toolbarButton.spec.ts
+++ src/components/toolbar-menu/toolbarButton.spec.ts
@@ -9,7 +9,8 @@ describe('ToolbarButton test', () =>  {
 
   describe('directive', () => {
     let scope,
-      compile;
+      compile,
+      compiledElement;
 
     const toolbarButtonData = require<string>('./toolbarButton.json');
 
@@ -19,33 +20,32 @@ describe('ToolbarButton test', () =>  {
         scope = $rootScope.$new();
         compile = $compile;
       });
+
+      scope.toolbarButtonData = toolbarButtonData;
+      compiledElement = compile(
+        angular.element(
+          `<miq-toolbar-button toolbar-button="toolbarButtonData"
+                                 on-item-click="onItemClick(item, $event)"></miq-toolbar-button>`
+        ))(scope);
+      scope.$digest();
     });
 
     it('creates button and checks for {name, confirm-tb, explorer, title, url, click, url_parms}', () => {
-      scope.toolbarButtonData = toolbarButtonData;
-      let compiledElement = compile(
-          angular.element(
-            `<miq-toolbar-button toolbar-button="toolbarButtonData"
-                                 on-item-click="onItemClick(item, $event)"></miq-toolbar-button>`
-          ))(scope);
-      scope.$digest();
       expect(compiledElement.attr('name')).toBe(scope.toolbarButtonData.name);
       expect(compiledElement.attr('data-confirm-tb')).toBe(scope.toolbarButtonData.confirm);
-      expect(!!compiledElement.attr('data-explorer')).toBe(scope.toolbarButtonData.explorer);
+      expect(JSON.parse(compiledElement.attr('data-explorer'))).toBe(scope.toolbarButtonData.explorer);
       expect(compiledElement.attr('title')).toBe(scope.toolbarButtonData.title);
       expect(compiledElement.attr('data-url')).toBe(scope.toolbarButtonData.url);
       expect(compiledElement.attr('data-click')).toBe(scope.toolbarButtonData.name);
       expect(compiledElement.attr('data-url_parms')).toBe(scope.toolbarButtonData.url_parms);
     });
 
+    it('toolbar button should contain text', () => {
+      expect(compiledElement.html()).toContain(scope.toolbarButtonData.text);
+    });
+
     it('create button with img instead of icon', () => {
-      scope.toolbarButtonData = toolbarButtonData;
       scope.toolbarButtonData.icon = null;
-      let compiledElement = compile(
-        angular.element(
-          `<miq-toolbar-button toolbar-button="toolbarButtonData"
-                                 on-item-click="onItemClick(item, $event)"></miq-toolbar-button>`
-        ))(scope);
       scope.$digest();
       expect(compiledElement.html()).toContain(`ng-src="${scope.toolbarButtonData.img_url}"`);
     });

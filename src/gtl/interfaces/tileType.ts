d63f62409ace1559c4981e781cfb4ec48cb22e33
Karel Hala <khala@redhat.com>
2016-09-21 11:38:53 +0200

    Add tests for limitToSuffix and add jsdoc to tileType

--- src/gtl/interfaces/tileType.ts
+++ src/gtl/interfaces/tileType.ts
@@ -1,8 +1,18 @@
 /**
- *
- * @type {{}}
+ * Enum for tile types. It holds string value of types for tiles.
+ * @memberof miqStaticAssets.gtl
+ * @ngdoc enum
+ * @name TileType
  */
 export const TileType  = {
+  /**
+   * Tile type: `small`
+   * @type {string}
+   */
   SMALL : 'small',
+  /**
+   * Tile type: `big`
+   * @type {string}
+   */
   BIG : 'big'
 };

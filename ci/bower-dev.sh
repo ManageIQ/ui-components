8a852d52855036690f5928303c28e2a33d479748
Martin Hradil <mhradil@redhat.com>
2017-07-13 16:17:08 +0000

    Debug problems generating bower-dev on travis
    ..by adding `git status` before commit and `git log -p -1` after.

--- ci/bower-dev.sh
+++ ci/bower-dev.sh
@@ -12,7 +12,9 @@ mv -f bower.json.tmp bower.json
 git add -f dist/css/ui-components.css dist/css/ui-components.css.map
 git add -f dist/js/ui-components.js dist/js/ui-components.js.map
 git add -f bower.json
+git status
 git commit -m "Automated build from ${HASH}"
+git log -p -1
 set +x
 git push -f https://$GITHUB_AUTH@github.com/ManageIQ/ui-components.git bower-dev &> /dev/null
 echo "git push origin HEAD:bower-dev"

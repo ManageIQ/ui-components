8889201328c7e110e49831da9a56a417be62ec5a
Chris Hale <chris.hale@me.com>
2017-07-26 08:33:34 -0600

    Updated spacing on demo controller

--- demo/controllers/dialogComponentController.ts
+++ demo/controllers/dialogComponentController.ts
@@ -1,25 +1,25 @@
 
 export default class DialogComponentController {
-    public dialog: any;
-    public showDialogData: boolean;
-    public dialogDataResults: any;
+  public dialog: any;
+  public showDialogData: boolean;
+  public dialogDataResults: any;
 
-     /* @ngInject */
-    constructor() {
-        const dialogFile = require('../data/dialog-data.json');
-        this.dialog = dialogFile.resources[0].content[0];
-        this.showDialogData = false;
-    }
-    public refreshField(field) {
-        let Promise: any;
-        return new Promise((resolve, reject) => {
-            resolve({'status': 'success'});
-        });
-    }
-    public dialogData(data) {
-       this.dialogDataResults = data;
-    }
-    public showDialogDataResults() {
-        this.showDialogData = true;
-    }
+  /* @ngInject */
+  constructor() {
+    const dialogFile = require('../data/dialog-data.json');
+    this.dialog = dialogFile.resources[0].content[0];
+    this.showDialogData = false;
+  }
+  public refreshField(field) {
+    let Promise: any;
+    return new Promise((resolve, reject) => {
+      resolve({ 'status': 'success' });
+    });
+  }
+  public dialogData(data) {
+    this.dialogDataResults = data;
+  }
+  public showDialogDataResults() {
+    this.showDialogData = true;
+  }
 }

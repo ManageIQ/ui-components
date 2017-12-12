77c7703415c596ce087b6df17c1d5708530e05e4
Martin Hradil <mhradil@redhat.com>
2017-12-12 19:41:38 +0000

    pf-sort - remove module, update directive to export a function

--- src/common/components/index.ts
+++ src/common/components/index.ts
@@ -1,5 +1,7 @@
 import SortItems from './sortItemsComponent';
+import miqPfSort from './miqPfSort';
 
 export default (module: ng.IModule) => {
   module.component('miqSortItems', new SortItems);
+  module.directive('miqPfSort', miqPfSort);
 };

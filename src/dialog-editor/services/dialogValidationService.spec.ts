ad11d373d828a41c8e5a1e87f878f6137225c169
Roman Blanco <rblanco@redhat.com>
2018-01-25 12:10:58 +0100

    Added specs for dialog's validation

---
+++ src/dialog-editor/services/dialogValidationService.spec.ts
@@ -0,0 +1,42 @@
+import DialogValidation from './dialogValidationService';
+import * as angular from 'angular';
+
+describe('DialogValidation test', () => {
+  let dialogValidation;
+  let dialogData;
+
+  beforeEach(() => {
+    angular.mock.inject(() => {
+      dialogValidation = new DialogValidation();
+    });
+
+  });
+
+  describe('#dialogIsValid validations for dialog', () => {
+    describe('when a dialog has no label', () => {
+      it('returns `false` and sets an error message', () => {
+        dialogData = [{
+          name: 'Larry\'s dialog',
+          dialog_tabs: [{
+            name: 'tab 1',
+            dialog_groups: []
+          }]
+        }];
+        expect(dialogValidation.dialogIsValid(dialogData)).toEqual(false);
+        expect(dialogValidation.invalid.message).toEqual('Dialog needs to have a label');
+      });
+    });
+    describe('when a dialog has no tab', () => {
+      it('returns `false` and sets an error message', () => {
+        dialogData = [{
+          label: 'this is a testing dialog and shouldn\'t be taken seriously',
+          name: 'Larry\'s dialog',
+          dialog_tabs: []
+        }];
+        expect(dialogValidation.dialogIsValid(dialogData)).toEqual(false);
+        expect(dialogValidation.invalid.message).toEqual('Dialog needs to have at least one tab');
+      });
+    });
+  });
+});
+

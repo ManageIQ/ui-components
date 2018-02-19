dd9b18b52815b556cbd6c9eefc0b39d82e3981ca
Karel Hala <khala@redhat.com>
2018-02-19 10:50:20 +0100

    Add bootstrap-select to vendor dependencies, fixes #260

--- src/vendor.ts
+++ src/vendor.ts
@@ -11,5 +11,6 @@ import 'angular-patternfly';
 import 'angular-ui-sortable';
 import 'angular-dragdrop';
 import 'angular-bootstrap-switch';
+import 'bootstrap-select';
 import 'ui-select';
 import 'patternfly-bootstrap-treeview';

60e86ac2258c4e1f15d63001e7de0f52c3485b6a
Milan Zazrivec <mzazrivec@redhat.com>
2018-07-24 17:57:30 +0200

    Allow angular expressions in gettext strings

--- scripts/validate-gettext-catalog.js
+++ scripts/validate-gettext-catalog.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const potFile = path.join(__dirname, '../locale/ui-components.pot')
 const contents = fs.readFileSync(potFile, 'utf8')
-const re = /<(.|\n)*>|{{(.|\n)*}}/gim // checks for html and also for javascript
+const re = /<(.|\n)*>/gim // checks for html
 const matches = contents.match(re)
 
 if (matches != null) {

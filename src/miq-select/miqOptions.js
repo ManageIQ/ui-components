fc47e62b755454d93eb308b7431e2b988d578929
Martin Hradil <mhradil@redhat.com>
2019-04-03 08:14:10 +0000

    miqOptions - update to evaluate expressions in option scope
    (we have to create such scope, but it works)
    
    So now we don't need to run functions against the DOM element, we can specify the value of data-tokens in a nice expression.

--- src/miq-select/miqOptions.js
+++ src/miq-select/miqOptions.js
@@ -1,5 +1,9 @@
 // use together with ng-options to set extra attributes on the option elements
-// use miq-options="{ attr1: 'static', attr2: vm.function }", where function gets run for each such element
+// use miq-options="{ attr1: 'static', attr2: expression, attr3: vm.function }"
+// expression gets evaluated in each option's scope separately,
+// function gets run with (scope, element)
+
+/* @ngInject */
 var miqOptions = function ($parse) {
   'use strict';
 
@@ -7,34 +11,69 @@ var miqOptions = function ($parse) {
     priority: 0,
     restrict: 'A',
     link: function (scope, element, attrs) {
-      var miqOptions = $parse(attrs.miqOptions)(scope);
-
-      var match = attrs.ngOptions.match(/for\s+.+\s+in\s+(.+)\s*/);
+      var match = attrs.ngOptions.match(/for\s+(.+)\s+in\s+(.+)\s*/);
       if (!match) {
+        console.warn('miqOptions: couldn\'t parse ngOptions', attrs.ngOptions);
         return;
       }
-      var attrToWatch = match[1];
+      var attrName = match[1];
+      var attrToWatch = match[2];
 
-      scope.$watch(attrToWatch, function (newValue) {
+      var parsedOptions = $parse(attrs.miqOptions);
+
+      var updateOptions = function (newValue) {
         if (!newValue) {
           return;
         }
 
-        angular.element('option', element).each(function (_i, e) {
-          Object.keys(miqOptions).forEach(function(k) {
-            var v = miqOptions[k];
-            if (_.isFunction(v)) {
-              v = v(e);
+        var values = [];
+        if (_.isArray(newValue)) {
+          values = newValue;
+        } else if (_.isObject(newValue)) {
+          values = Object.values(newValue);
+        } else {
+          console.warn('miqOptions: Unknown ngOptions source', newValue);
+          return;
+        }
+
+        var optionCount = angular.element('option', element).length;
+
+        var ignoredOptions = 0;
+        if (values.length === optionCount) {
+          ignoredOptions = 0;
+        } else if (values.length === optionCount - 1) {
+          ignoredOptions = 1;
+        } else {
+          console.warn('miqOptions: mismatch between number of items and options', values.length, optionCount);
+          return;
+        }
+
+        angular.element('option', element).each(function (i, optionElement) {
+          var idx = i - ignoredOptions;
+          if (idx < 0) {
+            return; // skip empty option
+          }
+
+          var optionScope = scope.$new();
+          optionScope.$index = idx;
+          optionScope[attrName] = values[idx];
+
+          var miqOptions = parsedOptions(optionScope);
+          Object.keys(miqOptions).forEach(function(key) {
+            var value = miqOptions[key];
+            if (_.isFunction(value)) {
+              value = value(optionScope, optionElement);
             }
 
-            angular.element(e).attr(k, v);
+            angular.element(optionElement).attr(key, value);
           });
         });
-      });
+      };
+
+      scope.$watch(attrToWatch, updateOptions);
+      scope.$watchCollection(attrToWatch, updateOptions);
     },
   };
 };
-
-miqOptions.$inject = ['$parse'];
 
 export default miqOptions;

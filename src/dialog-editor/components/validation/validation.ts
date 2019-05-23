3ae4ba1a82ed38e33f322dde40abefd3c2417151
Martin Hradil <mhradil@redhat.com>
2019-05-23 11:59:46 +0000

    dialog-editor-validation - chane validator_type false value from undefined to false
    to work around angular-bootstrap-switch parsing "undefined" as an indeterminate value (undecided switch)
    (https://github.com/frapontillo/angular-bootstrap-switch/issues/138)
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1705013

--- src/dialog-editor/components/validation/validation.ts
+++ src/dialog-editor/components/validation/validation.ts
@@ -3,4 +3,12 @@ export default {
   bindings: {
     modalData: '<',
   },
+  controller: function() {
+    this.$onChanges = () => {
+      if (this.modalData && ! this.modalData.validator_type) {
+        // prevent null or undefined being interpreted wrong by the switch
+        this.modalData.validator_type = false;
+      }
+    };
+  },
 };

4fbdf306e09958a8dbf1a662b3453fd6a18f193f
Martin Hradil <mhradil@redhat.com>
2020-06-30 19:21:57 +0200

    Merge pull request #443 from mzazrivec/add_support_for_local_validation_of_box_tab_and_field
    Add support for local validation of box, tab and field
    
    (cherry picked from commit 334a9ea4ef5124e4b9c0c35ec1f9b2703bcd0b33)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1741633

--- src/dialog-editor/services/dialogValidationService.ts
+++ src/dialog-editor/services/dialogValidationService.ts
@@ -12,44 +12,91 @@ export default class DialogValidationService {
     this.validators = {
       dialog: [
         dialog => ({ status: ! _.isEmpty(dialog.label),
-                     errorMessage: __('Dialog needs to have a label') }),
+                     errorMessage: __('Dialog needs to have a label'),
+                     local: true }),
         dialog => ({ status: dialog.dialog_tabs.length > 0,
-                     errorMessage: __('Dialog needs to have at least one tab') })
+                     errorMessage: __('Dialog needs to have at least one tab'),
+                     local: false })
       ],
       tabs: [
         tab => ({ status: ! _.isEmpty(tab.label),
-                  errorMessage: __('Dialog tab needs to have a label') }),
+                  errorMessage: __('Dialog tab needs to have a label'),
+                  local: true }),
         tab => ({ status: tab.dialog_groups.length > 0,
-                  errorMessage: __('Dialog tab needs to have at least one section') })
+                  errorMessage: __('Dialog tab needs to have at least one section'),
+                  local: false })
       ],
       groups: [
         group => ({ status: ! _.isEmpty(group.label),
-                    errorMessage: __('Dialog section needs to have a label') }),
+                    errorMessage: __('Dialog section needs to have a label'),
+                    local: true }),
         group => ({ status: group.dialog_fields.length > 0,
-                    errorMessage: __('Dialog section needs to have at least one field') })
+                    errorMessage: __('Dialog section needs to have at least one field'),
+                    local: false })
       ],
       fields: [
         field => ({ status: ! _.isEmpty(field.name),
-                    errorMessage: __('Dialog field needs to have a name') }),
+                    errorMessage: __('Dialog field needs to have a name'),
+                    local: true }),
         field => ({ status: ! _.isEmpty(field.label),
-                    errorMessage: __('Dialog field needs to have a label') }),
+                    errorMessage: __('Dialog field needs to have a label'),
+                    local: true }),
         field => ({ status: ! ((field.type === 'DialogFieldDropDownList' ||
                                 field.type === 'DialogFieldRadioButton')
                                && (!field.dynamic && _.isEmpty(field.values))),
-                    errorMessage: __('Dropdown needs to have entries') }),
+                    errorMessage: __('Dropdown needs to have entries'),
+                    local: true }),
         field => ({ status: (field.type !== 'DialogFieldTagControl') || tagHasCategory(field),
-                    errorMessage: __('Category needs to be set for TagControl field') }),
+                    errorMessage: __('Category needs to be set for TagControl field'),
+                    local: true }),
         field => ({ status: ! (field.dynamic && _.isEmpty(field.resource_action.ae_class)),
-                    errorMessage: __('Entry Point needs to be set for Dynamic elements') }),
+                    errorMessage: __('Entry Point needs to be set for Dynamic elements'),
+                    local: true }),
         field => ({ status: ! ((field.type === 'DialogFieldDropDownList' ||
                                 field.type === 'DialogFieldRadioButton')
                                && (field.data_type === 'integer')
                                && (!_.chain(field.values)
                                    .map(dialog_entries => _.toNumber(dialog_entries[0]))
                                    .every(value => !_.isNaN(value)).value())),
-                    errorMessage: __('Value type is set as Integer, but the value entered is not a number')}),
+                    errorMessage: __('Value type is set as Integer, but the value entered is not a number'),
+                    local: true }),
       ],
     };
+  }
+
+  private validate = (item, description, local = false) => ((fn) => {
+    let validation = fn(item);
+    if (local && !validation.local) {
+      return true;
+    }
+    if (! validation.status) {
+      Object.assign(this.invalid, {
+        item,
+        description,
+        message: validation.errorMessage,
+      });
+    }
+    return validation.status;
+  })
+
+  public validateDialog(dialog, local = false) {
+    const label = dialog.label ? sprintf(__('Dialog %s'), dialog.label) : __('Unnamed Dialog');
+    return _.every(this.validators.dialog, this.validate(dialog, label, local));
+  }
+
+  public validateTab(tab, local = false) {
+    const label = tab.label ? sprintf(__('Tab %s'), tab.label) : __('Unnamed Tab');
+    return _.every(this.validators.tabs, this.validate(tab, label, local));
+  }
+
+  public validateGroup(group, local = false) {
+    const label = group.label ? sprintf(__('Section %s'), group.label) : __('Unnamed Section');
+    return _.every(this.validators.groups, this.validate(group, label, local));
+  }
+
+  public validateField(field, local = false) {
+    const label = field.label ? sprintf(__('Field %s'), field.label) : __('Unnamed Field');
+    return _.every(this.validators.fields, this.validate(field, label, local));
   }
 
   /**
@@ -60,36 +107,14 @@ export default class DialogValidationService {
   public dialogIsValid(dialogData: any) {
     this.invalid.message = null;
 
-    const validate = (item, description) => ((fn) => {
-      let validation = fn(item);
-      if (! validation.status) {
-        Object.assign(this.invalid, {
-          item,
-          description,
-          message: validation.errorMessage,
-        });
-      }
-      return validation.status;
-    });
-
-    const describeDialog = (dialog) => dialog.label ? sprintf(__('Dialog %s'), dialog.label) : __('Unnamed Dialog');
-    const describeTab = (tab) => tab.label ? sprintf(__('Tab %s'), tab.label) : __('Unnamed Tab');
-    const describeGroup = (group) => group.label ? sprintf(__('Section %s'), group.label) : __('Unnamed Section');
-    const describeField = (field) => field.label ? sprintf(__('Field %s'), field.label) : __('Unnamed Field');
-
-    const validateDialog = (dialog) => _.every(this.validators.dialog, validate(dialog, describeDialog(dialog)));
-    const validateTab = (tab) => _.every(this.validators.tabs, validate(tab, describeTab(tab)));
-    const validateGroup = (group) => _.every(this.validators.groups, validate(group, describeGroup(group)));
-    const validateField = (field) => _.every(this.validators.fields, validate(field, describeField(field)));
-
     return _.every(dialogData, dialog =>
-      validateDialog(dialog) &&
+      this.validateDialog(dialog) &&
       _.every((<any>dialog).dialog_tabs, tab =>
-        validateTab(tab) &&
+        this.validateTab(tab) &&
         _.every((<any>tab).dialog_groups, group =>
-          validateGroup(group) &&
+          this.validateGroup(group) &&
           _.every((<any>group).dialog_fields, field =>
-            validateField(field)
+            this.validateField(field)
           )
         )
       )

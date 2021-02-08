9a4c31ecc242a1df40bc169df9a6594b60a77e09
Martin Hradil <mhradil@redhat.com>
2021-02-08 19:24:27 +0000

    remove unused miq-pf-sort, miq-sort-items

--- src/common/components/index.ts
+++
@@ -1,7 +0,0 @@
-import SortItems from './sortItemsComponent';
-import miqPfSort from './miqPfSort';
-
-export default (module: ng.IModule) => {
-  module.component('miqSortItems', new SortItems);
-  module.directive('miqPfSort', miqPfSort);
-};

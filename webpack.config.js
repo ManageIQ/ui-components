48a2e9b9ac6d0eb1e2706c01ae433b5e450e42b4
Martin Hradil <mhradil@redhat.com>
2021-02-08 19:44:05 +0000

    webpack - inline and cleanup settings, don't configure configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,25 @@
 "use strict";
-const settings = require('./application-settings.js');
-const webpack = require('webpack'),
-  path = require('path'),
-  NgAnnotatePlugin = require('ng-annotate-webpack-plugin'),
-  BrowserSyncPlugin = require('browser-sync-webpack-plugin'),
-  CopyWebpackPlugin = require('copy-webpack-plugin'),
-  ExtractTextPlugin = require('extract-text-webpack-plugin'),
-  HtmlWebpackPlugin = require('html-webpack-plugin'),
-  spa = require('browser-sync-spa');
+const webpack = require('webpack');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const NgAnnotatePlugin = require('ng-annotate-webpack-plugin');
+const spa = require('browser-sync-spa');
 
 module.exports = (env) => {
   let production = env && env.NODE_ENV === 'production';
   let test = env && env.test;
 
   const appEntry = {
-    [settings.appName]: [
-      settings.sassEntryPoint,
-      settings.tsEntryPoint,
-    ].concat(settings.tsModules),
+    'ui-components': [
+      './src/styles/ui-components.scss',
+      './src/index.ts',
+      './src/dialog-user/index.ts',
+      './src/miq-select/index.ts',
+      './src/tree-selector/index.ts',
+      './src/tree-view/index.ts',
+    ],
 
     'demo-app': [
       './demo/index.ts',
@@ -36,10 +38,10 @@ module.exports = (env) => {
       inject: 'body',
     }),
     production ? new webpack.optimize.CommonsChunkPlugin({
-      name: settings.appName,
-      filename: settings.javascriptFolder + '/' + settings.appName + settings.isMinified(production),
+      name: 'ui-components',
+      filename: 'js/ui-components.min.js',
     }) : undefined,
-    new ExtractTextPlugin(settings.stylesheetPath),
+    new ExtractTextPlugin('css/[name].css'),
     new NgAnnotatePlugin({add: true}),
     production ? new webpack.optimize.UglifyJsPlugin({
       warnings: false,
@@ -52,9 +54,9 @@ module.exports = (env) => {
     context: __dirname,
     entry: appEntry,
     output: {
-      path: settings.outputFolder,
+      path: __dirname + '/dist',
       publicPath: '.',
-      filename: settings.javascriptFolder + "/[name]" + settings.isMinified(production),
+      filename: 'js/[name]' + (production ? '.min.js' : '.js'),
     },
     resolve: {
       extensions: ['.ts', '.js'],
@@ -67,25 +69,25 @@ module.exports = (env) => {
     module: {
       rules: [
         {
-          test: /\.ts?$/,
-          exclude: /(node_modules|libs)/,
+          test: /\.ts$/,
+          exclude: /node_modules/,
           loader: 'tslint-loader',
           enforce: 'pre',
           options: {emitErrors: true},
         },
         {
           test: /\.ts$/,
-          exclude: /(node_modules|libs)/,
+          exclude: /node_modules/,
           loader: 'awesome-typescript-loader',
         },
         {
           test: /\.html$/,
-          exclude: /(node_modules|libs|dist|tsd)/,
+          exclude: /node_modules/,
           loader: 'raw-loader',
         },
         {
           test: /\.scss/,
-          exclude: /(node_modules|lib)/,
+          exclude: /node_modules/,
           loader: ExtractTextPlugin.extract({
             fallbackLoader: 'style-loader',
             loader: 'css-loader!sass-loader',
@@ -113,7 +115,7 @@ module.exports = (env) => {
       new BrowserSyncPlugin({
         host: 'localhost',
         port: 4000,
-        server: {baseDir: [__dirname + settings.distFolder]},
+        server: {baseDir: [__dirname + '/dist']},
         open: !test,
         middleware: [
           {
--- webpack.vendor.config.js
+++ webpack.vendor.config.js
@@ -1,35 +1,25 @@
-const settings = require('./application-settings.js');
-const production = process.argv.indexOf('--production') !== -1;
-
-var webpack = require('webpack'),
-  ExtractTextPlugin = require('extract-text-webpack-plugin'),
-  path = require('path'),
-  plugins = [
-      !production ? undefined : new webpack.ProvidePlugin({
-        $: "jquery",
-        jQuery: "jquery",
-        "window.jQuery": "jquery",
-        "angular": "angular",
-        "_": "lodash"
-      }),
-      new ExtractTextPlugin(settings.stylesheetPath)
-    ].filter(p => !!p);
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   context: __dirname,
   entry: {
-    vendor: ['./src/vendor.ts', settings.sassRootFolder + '/vendor.scss']
+    vendor: [
+      './src/vendor.ts',
+      './src/styles/vendor.scss',
+    ],
   },
-  plugins: plugins,
+  plugins: [
+    new ExtractTextPlugin('css/[name].css'),
+  ],
   module: {
     rules: [
-      {enforce: 'pre', test: /\.ts?$/, loader: 'tslint-loader', exclude: /(node_modules|libs)/},
+      {enforce: 'pre', test: /\.ts?$/, loader: 'tslint-loader', exclude: /node_modules/},
       { test: require.resolve('jquery'), loader: 'expose-loader?jQuery!expose-loader?$' },
-      {test: /\.ts$/, loaders: ['awesome-typescript-loader'], exclude: /(node_modules|libs)/},
-      {test: /\.html$/, loader: 'raw-loader', exclude: /(node_modules|libs|dist|tsd)/},
+      {test: /\.ts$/, loaders: ['awesome-typescript-loader'], exclude: /node_modules/},
+      {test: /\.html$/, loader: 'raw-loader', exclude: /node_modules/},
       {test: /\.(png|jpg|gif|svg|woff|ttf|eot)/, loader:  'url-loader?limit=20480'},
       // stylesheets
-      {test: /\.scss/, exclude: /(node_modules|lib)/, loader: ExtractTextPlugin.extract(
+      {test: /\.scss/, exclude: /node_modules/, loader: ExtractTextPlugin.extract(
         {
           fallbackLoader: 'style-loader',
           loader: 'css-loader!sass-loader'
@@ -46,8 +36,8 @@ module.exports = {
     ]
   },
   output: {
-    path: settings.outputFolder,
+    path: __dirname + '/dist',
     publicPath: '.',
-    filename: 'js/[name].js'
-  }
+    filename: 'js/[name].js',
+  },
 };

92ff8a0cc1109f87bff64e5f8042bac852753257
Martin Hradil <mhradil@redhat.com>
2021-02-08 19:26:40 +0000

    remove unused filters abbrNumber, adjustColor
    previously used by angular toolbars

--- src/common/filters/index.ts
+++
@@ -1,7 +0,0 @@
-import AbbrNumber from './abbrNumberFilter';
-import AdjustColor from './adjustColorFilter';
-
-export default (module: ng.IModule) => {
-  module.filter('abbrNumber', AbbrNumber.filter);
-  module.filter('adjustColor', AdjustColor.filter);
-};
